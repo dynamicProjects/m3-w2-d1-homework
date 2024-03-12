2c4a81fccc153a7135a49494678e8e8fcdb1efff
shruti-source <shrutishrivastav77@gmail.com>
2024-03-12 02:14:36 -0500

    insert query done

--- app.js
+++ app.js
@@ -1,47 +1,3 @@
-// var stats =[
-//     {
-//         'city': 'San Juan', 
-//         'zip': '00926', 
-//         'state': 'PR', 
-//         'income': '34781',
-//         'age': '44'
-//     },
-//     {
-//         'city': 'Corona', 
-//         'zip': '11368', 
-//         'state': 'NY', 
-//         'income': '50797',
-//         'age': '32'
-//     },
-//     {
-//         'city': 'Chicago', 
-//         'zip': '60629', 
-//         'state': 'IL', 
-//         'income': '42019',
-//         'age': '31'
-//     },
-//     {
-//         'city': 'El Paso', 
-//         'zip': '79936', 
-//         'state': 'TX', 
-//         'income': '54692',
-//         'age': '31'
-//     },
-//     {
-//         'city': 'Los Angeles', 
-//         'zip': '90011', 
-//         'state': 'CA', 
-//         'income': '36954',
-//         'age': '28'
-//     },
-//     {
-//         'city': 'Norwalk', 
-//         'zip': '90650', 
-//         'state': 'CA', 
-//         'income': '66453',
-//         'age': '35'
-//     }
-// ]
 const { MongoClient } = require('mongodb');
 const url = "mongodb://localhost:27017/nodemongo";
 
@@ -56,16 +12,62 @@ async function connectToDB() {
 
         // Access the "nodemongo" database
         const db = client.db("statsdb");
-        await db.createCollection("uscensus");
-        console.log("Collection 'uscensus' created");
-    //     var custData = [{ name: 'John', address: 'Highway 71'},
-    //     {'city': 'Pacoima', 'zip': '91331','state': 'CA','income': '60360','age': '33'},
-    //     {'city': 'Ketchikan', 'zip': '99950','state': 'AK','income': '00000','age': '00'},
-    //   ]
-    //    await db.collection("customers").insertMany(custData, function(err, res){
+        // await db.createCollection("uscensus");
+        // console.log("Collection 'uscensus' created");
+        // ------  insert query started-------- 
+        // var uscensusData = [
+                // {
+                //     'city': 'San Juan', 
+                //     'zip': '00926', 
+                //     'state': 'PR', 
+                //     'income': '34781',
+                //     'age': '44'
+                // },
+                // {
+                //     'city': 'Corona', 
+                //     'zip': '11368', 
+                //     'state': 'NY', 
+                //     'income': '50797',
+                //     'age': '32'
+                // },
+                // {
+                //     'city': 'Chicago', 
+                //     'zip': '60629', 
+                //     'state': 'IL', 
+                //     'income': '42019',
+                //     'age': '31'
+                // },
+                // {
+                //     'city': 'El Paso', 
+                //     'zip': '79936', 
+                //     'state': 'TX', 
+                //     'income': '54692',
+                //     'age': '31'
+                // },
+                // {
+                //     'city': 'Los Angeles', 
+                //     'zip': '90011', 
+                //     'state': 'CA', 
+                //     'income': '36954',
+                //     'age': '28'
+                // },
+                // {
+                //     'city': 'Norwalk', 
+                //     'zip': '90650', 
+                //     'state': 'CA', 
+                //     'income': '66453',
+                //     'age': '35'
+                // }
+        //          {'city': 'Pacoima', 'zip': '91331','state': 'CA','income': '60360','age': '33'},
+        // {'city': 'Ketchikan', 'zip': '99950','state': 'AK','income': '00000','age': '00'},
+            // ] 
+   
+    //    await db.collection("uscensus").insertMany(uscensusData, function(err, res){
     //         if(err) throw err;
     //         console.log("Number of document inserted: " + res.insertedCount)
     //     })
+     //  ----- insert query ended -----
+     
     // await db.collection("customers").findOne({}, function(err,result){
     //     if (err) throw err;
     //     console.log(result.name)
